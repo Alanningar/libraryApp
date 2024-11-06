759d191a4d46f448e24e0f68dd95b94b7aa44591
tanehans <117091615+tanehans@users.noreply.github.com>
2024-11-06 22:27:31 +0200

    signup page + api

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,10 @@
+// src/app/signup/page.tsx
+import Signup from "@/comps/Signup";
+
+export default function SignupPage() {
+    return (
+        <div>
+            <Signup />
+        </div>
+    );
+}
