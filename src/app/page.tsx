0137dd687f06e9a21b702790bbbdeea77dbaaff2
Alexander Kasianov <117091619+teethfrog@users.noreply.github.com>
2024-11-06 17:15:43 +0200

    Dynamic Books
    Böcker är nu tagna från databasen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Books from "../comps/BookView.jsx";
-
-
 export default function Home() {
   return (
    <h1> d=====(￣▽￣*)b </h1>
--- src/comps/BookView.jsx
+++ src/comps/BookView.jsx
@@ -1,12 +1,26 @@
-function Books({ books }) {
+"use client";
+
+import { useEffect, useState } from 'react';
+
+function BookView() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/book/')
+            .then(response => response.json())
+            .then(data => setBooks(data))
+            .catch(error => console.error('Error fetching books:', error));
+    }, []);
+
     return (
-        <div class="Books">
-            {books.map(({ image, title, author, releaseInfo }, i) => (
-                <div class="Book" key={i}>
-                    <img src={image} alt="bookCover" width={"300px"} height={"300px"}></img>
-                    <div class="BookInfo">
+        <div className="Books">
+            {books.map(({ cover, title, author, released, genre }, i) => (
+                <div className="Book" key={i}>
+                    <img className="BookCover" src={cover} alt="bookCover" />
+                    <div className="BookInfo">
                         <h1>{title}</h1>
-                        <h2>{author} | {releaseInfo} </h2>
+                        <h2>{author} | {released} </h2>
+                        <h3>{genre}</h3>
                     </div>
                 </div>
             ))}
@@ -14,4 +28,4 @@ function Books({ books }) {
     );
 }
 
-export default Books;
+export default BookView;
