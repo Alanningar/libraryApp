9b9770cbecf2ca875c25587267a4bf2d56d8d030
Alexander Kasianov <117091619+teethfrog@users.noreply.github.com>
2024-11-08 18:07:50 +0200

    Selector (Genre, Stock) + Tests

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,29 +1,18 @@
-import BookView from "@/comps/BookView";
+"use client";
 
-const books = [
-  {
-      image: "/images/book1.jpg",
-      title: "The Fractalist",
-      author: "Benoit Mandelbrot",
-      releaseInfo: "2021"
-  },
-  {
-      image: "/images/book2.jpg",
-      title: "Who Will Run The Frog Hospital?",
-      author: "Lorrie Moore",
-      releaseInfo: "2020"
-  },
-  {
-      image: "/images/book3.jpg",
-      title: "Winchell",
-      author: "Neal Gabler",
-      releaseInfo: "2019"
-  },
-  
-];
+import { useState } from 'react';
+import BookView from "@/comps/BookView";
+import { GenreSelector, StockFilter }  from "@/comps/Selector";
 
 export default function Home() {
+  const [selectedGenre, setSelectedGenre] = useState('none');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
-   <BookView />
+    <>
+      <GenreSelector selectedGenre={selectedGenre} setSelectedGenre={setSelectedGenre} />
+      <StockFilter inStockOnly={inStockOnly} setInStockOnly={setInStockOnly} />
+      <BookView selectedGenre={selectedGenre} inStockOnly={inStockOnly} />
+    </>
   );
 }
---
+++ src/comps/Selector.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+function GenreSelector({ selectedGenre, setSelectedGenre }) {
+    const handleChange = (e) => {
+        const genre = e.target.value;
+        console.log('Selected Genre:', genre);
+        setSelectedGenre(genre);
+    };
+
+    return (
+        <div class="SelectorDiv">
+            <select class="Selector" value={selectedGenre} onChange={handleChange}>
+                <option value="none">Select a genre</option>
+                <option value="fiction">Fiction</option>
+                <option value="mystery">Mystery</option>
+                <option value="fantasy">Fantasy</option>
+                <option value="science Fiction">Science Fiction</option>
+                <option value="romance">Romance</option>
+                <option value="paranormal">Paranormal</option>
+                <option value="thriller">Thriller</option>
+                <option value="action">Action</option>
+                <option value="historical">Historical</option>
+                <option value="psychology">Psychology</option>
+                <option value="self-help">Self-help</option>
+            </select>
+        </div>
+    );
+}
+
+function StockFilter({ inStockOnly, setInStockOnly }) {
+    const handleToggle = () => {
+        setInStockOnly(!inStockOnly);
+    };
+
+    return (
+        <div class="StockFilterDiv">
+            <label class="StockFilter">
+                <input class="StockFilterCheckbox" 
+                    type="checkbox"
+                    checked={inStockOnly}
+                    onChange={handleToggle}
+                />
+                Stock
+            </label>
+        </div>
+    );
+}
+
+export { GenreSelector, StockFilter };
